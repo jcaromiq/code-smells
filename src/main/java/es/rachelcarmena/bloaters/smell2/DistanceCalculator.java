aa58e85ddeef6ff85bd16040db064c59290bb64d
Joaquin Caro <me@joaquin-caro.es>
2017-07-23 18:24:49 +0200

    Refactor: Data clump smell with Extract parameter Object method

--- src/main/java/es/rachelcarmena/bloaters/smell2/DistanceCalculator.java
+++ src/main/java/es/rachelcarmena/bloaters/smell2/DistanceCalculator.java
@@ -1,11 +1,11 @@
 package es.rachelcarmena.bloaters.smell2;
 
 public class DistanceCalculator {
-    public double betweenPoints(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((y2 - y1), 2) + Math.pow(x2 - x1, 2));
+    public double betweenPoints(Point point1, Point point2) {
+        return Math.sqrt(Math.pow((point2.getY() - point1.getY()), 2) + Math.pow(point2.getX() - point1.getX(), 2));
     }
 
-    public double toOriginFrom(int x, int y) {
-        return Math.sqrt(Math.pow(y, 2) + Math.pow(x, 2));
+    public double toOriginFrom(Point point) {
+        return Math.sqrt(Math.pow(point.getY(), 2) + Math.pow(point.getX(), 2));
     }
 }
